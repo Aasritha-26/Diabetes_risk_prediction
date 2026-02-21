c4a92cc3ffa1147bc213f369f2bfc996f1a88fa7
Aasritha_nagulakonda <nagulakondaaasritha@gmail.com>
2026-02-22 01:38:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import pandas as pd
 transformer = pickle.load(open('XG_transformer.pkl', 'rb'))
 model = pickle.load(open('XG_model.pkl', 'rb'))
 
+st.write("Model object:", model)
+st.write("Model booster params:", model.get_params())
 st.header("Diabetes Risk Prediction")
 
 st.write("Enter patient details to predict diabetes risk")
@@ -53,3 +55,4 @@ if submit:
     risk_score = model.predict(trans_data)[0]
 
     st.write(f"Risk Score: {risk_score:.2f}")
+
